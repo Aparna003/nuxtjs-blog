2465999bf44fbaad372df746ac474a2e3f2d59d3
Aparna003 <aparnashankar004@gmail.com>
2020-10-12 21:31:14 +0530

    Added vuex store

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -44,6 +44,10 @@ export default {
        context.error(new Error())
      });
    },
+   created(){
+     this.$store.dispatch('setPosts', this.loadedPosts)
+     console.log(this.$store.getters.loadedPosts)
+   }
 }
 </script>
 
