364848f251ebc910eb98c3e19baf8056ef27ab80
Aparna003 <aparnashankar004@gmail.com>
2020-10-12 23:29:37 +0530

    Post using axios to store data in firebase

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="admin-new-post-pg">
     <section class="new-post-form">
-       <AdminFormPost />
+       <AdminFormPost @submit="onSubmitted" />
     </section>
   </div>
 </template>
 
 <script>
 import AdminFormPost from '@/components/Admin/AdminFormPost'
-
+import axios from 'axios'
 export default {
   components : {
     AdminFormPost
+  },
+  methods:{
+    onSubmitted(postData){
+       axios.post('https://nuxtvue-blog.firebaseio.com/posts.json',postData)
+       .then(result=>console.log(result))
+       .catch(e => console.log(e))
+    }
   }
 };
 </script>
